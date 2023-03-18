131d3b6800e422a90a9e5067692472315040adcc
Vamsi Kurli <n00466003@humbermail.com>
2023-03-18 05:04:11 -0400

    KVP Pictures

---
+++ Javascript/javascript.js
@@ -0,0 +1,6 @@
+window.alert("Hello, Welcome to KVP Pictures (Official site)")
+function Showtext() {
+    console.log("it worked")
+    document.getElementById("intromessage").style.display = "contents"
+}
+
