7aa758d5c38dec5991e919f3858b27a525f93a5e
Vamsi Kurli <n00466003@humbermail.com>
2024-01-05 15:05:38 -0500

    New Update
    New update

--- Javascript/javascript.js
+++
@@ -1,6 +0,0 @@
-window.alert("Hello, Welcome to KVP Pictures (Official site)")
-function Showtext() {
-    console.log("it worked")
-    document.getElementById("intromessage").style.display = "contents"
-}
-
